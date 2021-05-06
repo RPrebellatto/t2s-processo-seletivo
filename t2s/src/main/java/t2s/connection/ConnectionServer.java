515af5b1263665a91f99910c7127810639a11d78
RPRebellatto <rogerio.rebellatto@gmail.com>
2021-05-06 16:53:10 -0300

    Added select list to confirm db content

--- t2s/src/main/java/t2s/connection/ConnectionServer.java
+++ t2s/src/main/java/t2s/connection/ConnectionServer.java
@@ -3,6 +3,7 @@ package t2s.connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -11,7 +12,7 @@ public class ConnectionServer {
 	public static void main(String[] args) {
 		String jbdcURL = "jdbc:postgresql://127.0.0.1:5432/t2s_pseletivo";
 		String username = "postgres";
-		String password = "";
+		String password = "admin";
 		
 		try {
 			Connection connection = DriverManager.getConnection(jbdcURL,username,password);
@@ -20,19 +21,35 @@ public class ConnectionServer {
 			String sql = "INSERT INTO container (cliente, numero_container, tipo_20_40, status_vazio_cheio, categoria_imp_exp)"
 					+ "VALUES (?,?,?,?,?)";
 			
-			PreparedStatement statement = connection.prepareStatement(sql);
+			PreparedStatement preparedstatement = connection.prepareStatement(sql);
 			
-			statement.setString(1, "MSC");
-			statement.setString(2, "FWEF2324123");
-			statement.setInt(3, 40);
-			statement.setString(4, "cheio");
-			statement.setString(5, "exportação");
+			preparedstatement.setString(1, "MSC");
+			preparedstatement.setString(2, "POPA5584123");
+			preparedstatement.setInt(3, 40);
+			preparedstatement.setString(4, "cheio");
+			preparedstatement.setString(5, "exportação");
 			
-			int rows = statement.executeUpdate();
+			int rows = preparedstatement.executeUpdate();
 			if(rows > 0) {
 				System.out.println("Foi feita uma inclusão no banco");
 			}
 			
+			sql = "SELECT * FROM container";
+			
+			Statement statement = connection.createStatement();
+			
+			ResultSet result = statement.executeQuery(sql);
+			
+			while (result.next()) {
+				String cliente = result.getString("cliente");
+				String numero = result.getString("numero_container");
+				int tipo = result.getInt("tipo_20_40");
+				String status = result.getString("status_vazio_cheio");
+				String categoria = result.getString("categoria_imp_exp");
+				
+				System.out.printf("%s %s %d %s %s \n", cliente, numero, tipo, status, categoria);
+			}
+
 			connection.close();
 		} catch (Exception e) {
 			System.out.println("Erro ao tentar conexão com o banco de dados");
